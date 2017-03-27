6a7738a3a202a3061f82959aecbd680add4b0986
phdafoe <formador@aula8PovMacNfor.local>
2017-03-27 21:59:55 +0200

    Ajustes finales de la App de Internacionalización _ 2

--- App_Internacionalizacion/ViewController.swift
+++ App_Internacionalizacion/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var myTextoLBL : UILabel!
+    @IBOutlet weak var myNumeroLBL : UILabel!
+    @IBOutlet weak var myMonedaLBL : UILabel!
+    @IBOutlet weak var myFechaLBL : UILabel!
+    @IBOutlet weak var myImageView : UIImageView!
+    
+    //MARK: - LIFE VC
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        dameDatos()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        muestraAlertVCIdiom()
     }
-
-
+    
+    //MARK: - Utils
+    func dameDatos(){
+        myTextoLBL.text = NSLocalizedString("GOOD_MORNING", comment: "")
+        myNumeroLBL.text = "\(9.9999)"
+        myMonedaLBL.text = "\(50.0001)"
+        myFechaLBL.text = "\(Date())"
+        myImageView.image = UIImage(named: "hello.png")
+    }
+    
+    func muestraAlertVCIdiom(){
+        let alertVC = UIAlertController(title: NSLocalizedString("TITLE_DATA", comment: ""), message: NSLocalizedString("MESSAGE_DATA", comment: ""), preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertVC, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
